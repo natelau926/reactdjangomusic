6d464725790fdf9e17717359e7da1ba338d07415
nate <nathir0926@gmail.com>
2020-12-29 17:35:06 -0800

    first commit

---
+++ music_controller/frontend/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import HomePage from "./HomePage";
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div class="center">
+      <HomePage />
+      
+
+    </div>
+
+    )
+  }
+}
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
